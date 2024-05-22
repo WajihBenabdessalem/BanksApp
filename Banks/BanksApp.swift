5ffe4e0e2a362f7cf9bbac43b1d400651aa7eb47
Wajih Benabdessalem <benabdessalemwajih1212@gmail.com>
2024-05-22 20:56:30 +0200

    Initial Commit

---
+++ Banks/BanksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BanksApp.swift
+//  Banks
+//
+//  Created by Wajih Benabdessalem on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BanksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
